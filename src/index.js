96d9af47e5740f22737c568f8d1b19f024c41c6f
Joao Victor <jov9051@outlook.com>
2021-11-29 16:49:31 -0300

    route /all created

--- src/index.js
+++ src/index.js
@@ -6,6 +6,25 @@ const app = express();
 
 const PORT = 3000;
 
+let posts = [
+    {
+        id: 0,
+        title: "Mural de teste",
+        description: "Descrição teste"
+    },
+    {
+        id: 1,
+        title: "Mural teste 2",
+        description: "Descrição teste 2"
+    }
+];
+
+app.get("/all", (req, res) => {
+    res.send(JSON.stringify(posts));
+});
+
+app.post("/new", (req, res) => {});
+
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
 });
