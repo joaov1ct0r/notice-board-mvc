69f3b44126da35692b188a08861ad90c8460c591
Joao Victor <jov9051@outlook.com>
2022-01-04 11:46:44 -0300

    mysql connection started

--- src/model/posts.js
+++ src/model/posts.js
@@ -1,6 +1,13 @@
-module.exports = {
-    posts: [],
+const mysql = require('mysql2');
 
+let db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'password',
+    database: 'muralAvisos'
+});
+
+module.exports = {
     getAll() {
         return this.posts;
     },
