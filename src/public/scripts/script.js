67cf9bcbecd440e1706ec04ed29a6386e52c08f9
Joao Victor <jov9051@outlook.com>
2022-01-04 15:57:27 -0300

    button remover working on front and back-end

--- src/public/scripts/script.js
+++ src/public/scripts/script.js
@@ -18,13 +18,10 @@ function updatePosts() {
 
             posts.forEach(post => {
                 let postElement = `<div id="${post.avisosID}" class="card mb-5">
-                                          <div class="card-header">
                                               <h5 class="card-title">${post.avisosTitulo}</h5>
-                                          </div>
-                                          <div class="card-body">
-                                              <div class="card-text">${post.avisosDesc}</div>
-                                          </div>
-                                      </div>`;
+                                              ${post.avisosDesc}
+                                              <button>Editar</button><button>Remover</button>
+                                   </div>`;
 
                 postElements += postElement;
             });
@@ -64,3 +61,77 @@ function newPost() {
         document.getElementById('desc').value = '';
     });
 }
+
+let divPosts = document.getElementById('posts');
+
+divPosts.addEventListener('click', event => {
+    if (event.target.tagName === 'BUTTON') {
+        const button = event.target;
+
+        const postDiv = button.parentNode;
+
+        const divPosts = postDiv.parentNode;
+
+        if (button.textContent === 'Remover') {
+            const url = `http://localhost:3001/api/delete/${postDiv.id}`;
+
+            const options = {
+                method: 'DELETE',
+                body: null,
+                headers: { 'Content-type': 'application/json; charset=UTF-8' }
+            };
+
+            fetch(url, options).then(res => {
+                console.log(res);
+            });
+
+            divPosts.removeChild(postDiv);
+
+            updatePosts();
+        } else if (button.textContent === 'Editar') {
+            let title = document.createElement('input');
+
+            title.type = 'text';
+
+            title.value = postDiv.avisosTitulo;
+
+            divPosts.insertBefore(title, postDiv.lastChild);
+
+            let desc = document.createElement('input');
+
+            desc.type = 'text';
+
+            desc.value = postDiv.avisosDesc;
+
+            divPosts.insertBefore(desc, postDiv.lastChild);
+
+            button.textContent = 'Salvar';
+
+            button.addEventListener('click', () => {
+                editPost();
+            });
+
+            function editPost() {
+                const url = `http://localhost:3001/api/edit/${postDiv.avisosID}`;
+
+                const title = title.value;
+
+                const desc = desc.value;
+
+                const options = {
+                    method: 'PUT',
+                    body: JSON.stringify({ title }),
+                    headers: {
+                        'Content-type': 'application/json; charset=UTF-8'
+                    }
+                };
+
+                fetch(url, options).then(res => {
+                    console.log(res);
+
+                    updateToDo();
+                });
+            }
+        }
+    }
+});
