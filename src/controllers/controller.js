1d3491081b586c38af6171b1a7cfd08cc8f7e5ca
Joao Victor <jov9051@outlook.com>
2022-02-04 17:44:53 -0300

    funcao newPost criada em user dentro de controller.js

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -5,6 +5,16 @@ let user = {
         db.getAll(function (result) {
             res.send(JSON.stringify(result));
         });
+    },
+
+    newPost: function (req, res) {
+        let { title, description } = req.body;
+
+        db.newPost(title, description, function (result) {
+            console.log(result);
+
+            res.send('Aviso Adicionado com sucesso');
+        });
     }
 };
 
