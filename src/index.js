7b14091b667211a5dfadefce1a5fbb4132d504ed
Joao Victor <jov9051@outlook.com>
2022-02-04 17:36:27 -0300

    rota / utilizando index.html da pasta view

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const apiRoutes = require('./routes/api');
 
 app.use('/api', apiRoutes);
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'view')));
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`);
