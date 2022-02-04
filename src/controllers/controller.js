90e3e449ecd0b8b169e2a0621b3589172c717b49
Joao Victor <jov9051@outlook.com>
2022-02-04 17:43:11 -0300

    funcao getAll criada em user em controller.js

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,5 +1,11 @@
 const db = require('../model/db');
 
-let user = {};
+let user = {
+    getAll: function (req, res) {
+        db.getAll(function (result) {
+            res.send(JSON.stringify(result));
+        });
+    }
+};
 
 module.exports = user;
