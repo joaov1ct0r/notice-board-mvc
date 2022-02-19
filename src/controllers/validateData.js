b7462b075de3911fc0cb162a909e27c094def57c
Joao Victor <jov9051@outlook.com>
2022-02-19 14:12:55 -0300

    validateNewPost validando dados em validateData.js

--- src/controllers/validateData.js
+++ src/controllers/validateData.js
@@ -1,5 +1,10 @@
 const Joi = require('@hapi/joi');
 
 let validateNewPost = data => {
-    let schema = Joi.object({});
+    let schema = Joi.object({
+        title: Joi.string().required().min(2).max(250),
+        description: Joi.string().required().min(2).max(250)
+    });
+
+    return schema.validate(data);
 };
