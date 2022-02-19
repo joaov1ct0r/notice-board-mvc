1ab10fbc642b8f26f15a6ea8cd8f44cbf0d69523
Joao Victor <jov9051@outlook.com>
2022-02-19 14:15:01 -0300

    funcao validateChangePost validando dados em validateData.js

--- src/controllers/validateData.js
+++ src/controllers/validateData.js
@@ -10,5 +10,10 @@ let validateNewPost = data => {
 };
 
 let validateChangePost = data => {
-    let schema = Joi.object({});
+    let schema = Joi.object({
+        title: Joi.string().required().min(2).max(250),
+        description: Joi.string().required().min(2).max(250)
+    });
+
+    return schema.validate(data);
 };
