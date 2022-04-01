ad4e2c13f22cbb77d8bd830753a1a526aef66a26
Joao Victor <jov9051@outlook.com>
2022-04-01 17:06:36 -0300

    feat: modelo de db definido em userModel.js

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,71 +1,84 @@
-const mysql = require('mysql2');
+import { DataTypes } from 'sequelize';
 
-let db = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'password',
-    database: 'muralAvisos'
-});
+import dbConnection from '../config/db.js';
 
-// RETORNA TODOS OS AVISOS
-let getAll = callback => {
-    let SQL = `SELECT * FROM avisos`;
-
-    db.query(SQL, (err, result) => {
-        if (err) {
-            throw err;
+const Avisos = dbConnection.define(
+    'avisos',
+    {
+        avisosID: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        avisosTitulo: {
+            type: DataTypes.STRING(250),
+            allowNull: false
+        },
+        avisosDesc: {
+            type: DataTypes.STRING(250),
+            allowNull: false
         }
-        callback(result);
-    });
-};
+    },
+    {
+        freezeTableName: true,
+        tableName: 'avisos',
+        timestamps: false
+    }
+);
 
-// INSERE NOVO AVISO
-let newPost = (title, description, callback) => {
-    let SQL = `INSERT INTO avisos (avisosTitulo, avisosDesc) VALUES (?, ?)`;
+Avisos.sync();
 
-    let params = [title, description];
+export default Avisos;
 
-    db.query(SQL, params, (err, result) => {
-        if (err) {
-            throw err;
-        }
+// // RETORNA TODOS OS AVISOS
+// let getAll = callback => {
+//     let SQL = `SELECT * FROM avisos`;
 
-        callback(result);
-    });
-};
+//     db.query(SQL, (err, result) => {
+//         if (err) {
+//             throw err;
+//         }
+//         callback(result);
+//     });
+// };
 
-// MODIFICA UM AVISO
-let changePost = (index, title, description, callback) => {
-    let SQL = `UPDATE avisos SET avisosTitulo = ?, avisosDesc = ? WHERE avisosID = ?`;
+// // INSERE NOVO AVISO
+// let newPost = (title, description, callback) => {
+//     let SQL = `INSERT INTO avisos (avisosTitulo, avisosDesc) VALUES (?, ?)`;
 
-    let params = [title, description, index];
+//     let params = [title, description];
 
-    db.query(SQL, params, (err, result) => {
-        if (err) {
-            throw err;
-        }
+//     db.query(SQL, params, (err, result) => {
+//         if (err) {
+//             throw err;
+//         }
 
-        callback(result);
-    });
-};
+//         callback(result);
+//     });
+// };
 
-let deletePost = (index, callback) => {
-    let SQL = `DELETE FROM avisos WHERE avisosID = ?`;
+// // MODIFICA UM AVISO
+// let changePost = (index, title, description, callback) => {
+//     let SQL = `UPDATE avisos SET avisosTitulo = ?, avisosDesc = ? WHERE avisosID = ?`;
 
-    db.query(SQL, index, (err, result) => {
-        if (err) {
-            throw err;
-        }
-        callback(result);
-    });
-};
+//     let params = [title, description, index];
 
-module.exports = {
-    getAll,
+//     db.query(SQL, params, (err, result) => {
+//         if (err) {
+//             throw err;
+//         }
 
-    newPost,
+//         callback(result);
+//     });
+// };
 
-    changePost,
+// let deletePost = (index, callback) => {
+//     let SQL = `DELETE FROM avisos WHERE avisosID = ?`;
 
-    deletePost
-};
+//     db.query(SQL, index, (err, result) => {
+//         if (err) {
+//             throw err;
+//         }
+//         callback(result);
+//     });
+// };
