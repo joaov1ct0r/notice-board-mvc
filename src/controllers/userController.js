c4440fa264a753f071acc04e1a00089dacc729fd
Joao Victor <jov9051@outlook.com>
2022-04-01 17:22:43 -0300

    feat: deletePost deletando avisos

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,74 +2,86 @@ import Avisos from '../model/userModel.js';
 
 import { validateChangePost, validateNewPost } from './validateData';
 
-    const getAll = async (req, res) => {
-        try {
-            const avisos = await Avisos.findAll();
+const getAll = async (req, res) => {
+    try {
+        const avisos = await Avisos.findAll();
 
-            if(!avisos) return res.status(400).send('Falha ao obter dados!');
+        if (!avisos) return res.status(400).send('Falha ao obter dados!');
 
-            res.send(JSON.stringify(avisos));
-        } catch (error) {
-            throw error;
-        }
+        res.send(JSON.stringify(avisos));
+    } catch (error) {
+        throw error;
     }
+};
 
-    const newPost = async (req, res) => {
-        let { error } = validateNewPost(req.body);
+const newPost = async (req, res) => {
+    let { error } = validateNewPost(req.body);
 
-        if (error) return res.status(400).send('Falha na autenticação');
+    if (error) return res.status(400).send('Falha na autenticação');
 
-        let { title, description } = req.body;
+    let { title, description } = req.body;
 
-        try {
-            const aviso = await Avisos.create({
+    try {
+        const aviso = await Avisos.create({
+            avisosTitulo: title,
+            avisosDesc: description
+        });
+
+        if (!aviso)
+            return res.status(400).send('Falha ao adicionar novo aviso!');
+
+        res.send('Aviso adicionado com sucesso!');
+    } catch (error) {
+        throw error;
+    }
+};
+
+const changePost = async (req, res) => {
+    let { error } = validateChangePost(req.body);
+
+    if (error) return res.status(400).send('Falha na autenticação');
+
+    let { index } = req.params;
+
+    let { title, description } = req.body;
+
+    try {
+        const aviso = await Avisos.update(
+            {
                 avisosTitulo: title,
                 avisosDesc: description
-            })
-
-            if(!aviso) return res.status(400).send('Falha ao adicionar novo aviso!')
-
-            res.send("Aviso adicionado com sucesso!")
-        } catch (error) {
-            throw error;
-        }
-    }
-
-    const changePost = async (req, res) => {
-        let { error } = validateChangePost(req.body);
-
-        if (error) return res.status(400).send('Falha na autenticação');
-
-        let { index } = req.params;
-
-        let { title, description } = req.body;
-
-        try {
-            const aviso = await Avisos.update({
-                avisosTitulo: title,
-                avisosDesc: description
-            }, {
+            },
+            {
                 where: {
                     avisosID: index
                 }
-            });
+            }
+        );
 
-            if (!aviso) return res.status(400).send('Falha ao atualizar aviso!')
+        if (!aviso) return res.status(400).send('Falha ao atualizar aviso!');
 
-            res.send('Aviso atualizado com sucesso!')
-        } catch (error) {
-            throw error;
-        }
+        res.send('Aviso atualizado com sucesso!');
+    } catch (error) {
+        throw error;
     }
+};
 
-    deletePost(req, res) {
-        let { index } = req.params;
+const deletePost = async (req, res) => {
+    let { index } = req.params;
 
-        db.deletePost(index, function (result) {
-            console.log(result);
-
-            res.send('Aviso deletado com sucesso');
+    try {
+        const aviso = await Avisos.destroy({
+            where: {
+                avisosID: index
+            }
         });
+
+        if (!aviso) return res.status(400).send('Falha ao deletar aviso!');
+
+        res.send('Aviso deletado com sucesso');
+    } catch (error) {
+        throw error;
     }
+};
 
 module.exports = user;
